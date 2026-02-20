9d2738330cfaa4066886ad068d5563187fff2aa3
Jose Augusto <jose_augusto2019@hotmail.com>
2026-02-20 18:11:29 -0300

    feat: criando os enums que vao servir como tipo de movimentacao e tipo de produtos

---
+++ src/main/java/br/com/nexdom/estoque_backend/domain/enums/TipoMovimentacao.java
@@ -0,0 +1,6 @@
+package br.com.nexdom.estoque_backend.domain.enums;
+
+public enum TipoMovimentacao {
+    ENTRADA,
+    SAIDA
+}
---
+++ src/main/java/br/com/nexdom/estoque_backend/domain/enums/TipoProduto.java
@@ -0,0 +1,7 @@
+package br.com.nexdom.estoque_backend.domain.enums;
+
+public enum TipoProduto {
+    ELETRONICO,
+    ELETRODOMESTICO,
+    MOVEL
+}
